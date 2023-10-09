1b344a153f0f9c390397750542f496c1dacc51ca
Greg <greg.kabakian@revstarconsulting.com+1-at-099168194229>
2023-10-08 21:25:24 -0500

    fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    // Use the static adapter
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    // Set the base and assets path for GitHub Pages
+    paths: {
+        base: '/Greg-Portfolio', // assuming your repo name is 'portfolio'
+        assets: '/portfolio'
+    },
+    target: '#svelte'
   },
   preprocess: vitePreprocess()
 };
+
 export default config;
