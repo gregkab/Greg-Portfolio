5aa57d728fae931dd50358193d2e4df297fd267a
Greg <greg.kabakian@revstarconsulting.com+1-at-099168194229>
2023-10-16 15:32:15 -0700

    Revert "fixes"
    This reverts commit 1b344a153f0f9c390397750542f496c1dacc51ca.

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // Use the static adapter
-    adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
-    // Set the base and assets path for GitHub Pages
-    paths: {
-        base: '/Greg-Portfolio', // assuming your repo name is 'portfolio'
-        assets: '/portfolio'
-    },
-    target: '#svelte'
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };
-
 export default config;
